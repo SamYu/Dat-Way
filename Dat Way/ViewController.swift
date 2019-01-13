83bd5db3ec9270a08ec37d3ed44209c4878ee0fb
Tony Wang <tonyy.wangg@gmail.com>
2019-01-12 23:11:02 -0500

    Initial Commit

---
+++ Dat Way/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dat Way
+//
+//  Created by Tony Wang on 2019-01-12.
+//  Copyright © 2019 Tony Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
